4304d3570f622c61ea1ee6a73b79ac44d61af397
Halsey Quinn <hquinn@bdtrust.org>
2024-04-05 07:18:23 -0400

    add button animation to play if user has not engaged since last page refresh

--- widget/src/Widget.js
+++ widget/src/Widget.js
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 import WidgetWindow from './WidgetWindow';
 import WidgetButton from './WidgetButton';
-
-import { getCookieValue } from '../helpers/uuidHelpers';
 
 const Widget = ({ introMessage, onClick, sessionId }) => {
     const [open, setOpen] = useState(false);
     const [hasEngaged, setHasEngaged] = useState(false);
 
     const handleButtonClick = () => {
-        console.log('handling button click open state');
         if (typeof onClick === 'function') onClick();
         setOpen(!open);
+        if (!hasEngaged) setHasEngaged(true);
     }
 
-    useEffect(() => {
-        setHasEngaged(getCookieValue('BDT_ChatBot_User_UUID').length > 0);
-    }, [])
-
-    console.log(open, 'open state')
-
-    return <div className='complete-widget__container'>
-        <WidgetWindow introMessage={introMessage} sessionId={sessionId} open={open} />
-        <WidgetButton open={open} handleButtonClick={handleButtonClick} hasEngaged={hasEngaged} />
-    </div>;
+    return (
+        <div className='complete-widget__container'>
+            <WidgetWindow introMessage={introMessage} sessionId={sessionId} open={open} />
+            <WidgetButton open={open} handleButtonClick={handleButtonClick} hasEngaged={hasEngaged} />
+        </div>
+    )
 }
 
 export default Widget;
--- widget/src/WidgetButton.js
+++ widget/src/WidgetButton.js
@@ -6,6 +6,8 @@ import closeIcon from './images/close-icon.svg';
 const WidgetButton = ({ open, handleButtonClick, hasEngaged }) => {
     const [pillVisible, setPillVisible] = useState(false);
 
+    console.log(hasEngaged, 'has engaged value')
+
     useEffect(() => {
         const toggleVisibility = () => {
             setPillVisible(true);
