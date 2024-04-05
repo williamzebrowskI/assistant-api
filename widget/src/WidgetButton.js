2f451f1e24cd48596414d42b9605442b9deba860
Halsey Quinn <hquinn@bdtrust.org>
2024-04-05 01:17:04 -0400

    style open button

--- widget/src/WidgetButton.js
+++ widget/src/WidgetButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import wyattAvatar from './images/wyatt-chat-avatar.svg';
+import closeIcon from './images/close-icon.svg';
 
 const WidgetButton = ({ open, handleButtonClick }) => {
 
@@ -8,14 +9,16 @@ const WidgetButton = ({ open, handleButtonClick }) => {
         <button className='widget-button' name={open ? 'Close chat' : 'Open chat'} onClick={handleButtonClick}>
             <div className='widget-button__wrapper'>
                 {open ?
-                    <img aria-hidden={true} />
+                    <div className='widget-button__close'>
+                        <img className='widget-button__close' aria-hidden={true} src={closeIcon} />
+                    </div>
                     :
                     <img aria-hidden={true} src={wyattAvatar} />
                 }
             </div>
-            <div className='widget-button__pill-cta'>
+            {/* <div className='widget-button__pill-cta'>
                 Chat Now
-            </div>
+            </div> */}
         </button>
     )
 }
