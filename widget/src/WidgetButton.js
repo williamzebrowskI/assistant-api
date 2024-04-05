39ff2a45ffc0aba05926179e35ab72633ef9c044
Halsey Quinn <hquinn@bdtrust.org>
2024-04-05 00:38:03 -0400

    complete styling of window UI

---
+++ widget/src/WidgetButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WidgetButton = ({ open }) => {
+    return (
+        <button className='widget-button'>
+            <div className='widget-button__wrapper'>
+
+            </div>
+        </button>
+    )
+}
+
+export default WidgetButton;
