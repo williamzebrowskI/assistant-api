a5f33d174e2ed4cd54fdd8f2372dbe0070d86b32
William Zebrowski <38896109+williamzebrowskI@users.noreply.github.com>
2024-04-19 13:57:14 -0400

    elastic connector file

---
+++ managers/elastic/elastic_connector.py
@@ -0,0 +1,24 @@
+# Purpose: Establishes connection to Elasticsearch and contains the BaseElasticConnector class which includes basic connection setup and error logging specific to Elasticsearch operations.
+# Contents: Initialization of the Elasticsearch client, log_error method.
+
+import os
+import logging
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+load_dotenv()
+
+class BaseElasticConnector:
+    def __init__(self):
+        self.es_url = os.getenv('ES_URL', 'localhost')
+        self.es_port = os.getenv('ES_PORT', 9200)
+        self.es_api_key = os.getenv('ES_API_KEY')
+        self.es_index = os.getenv('ES_INDEX', 'conversations')
+
+        try:
+            self.es = Elasticsearch(
+                hosts=[f"{self.es_url}:{self.es_port}"],
+                api_key=self.es_api_key
+            )
+        except Exception as e:
+            logging.error("Failed to connect to Elasticsearch: %s", e)
+            raise SystemExit("Connection to Elasticsearch failed: %s" % e)
