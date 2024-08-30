1cd33ce46cde065173150c33bf1b5da76e00fad2
William Zebrowski <38896109+williamzebrowskI@users.noreply.github.com>
2024-08-30 00:07:59 -0400

    add frontend dir plus packages for widget

--- start.sh
+++ start.sh
@@ -2,12 +2,24 @@
 
 # Set the PYTHONPATH
 export PYTHONPATH=/usr/src/app
-
 export PYTHONDONTWRITEBYTECODE=1
 
 # Set Elasticsearch enabled/disabled
-export ELASTICSEARCH_ENABLED=false  # false to disable Elastic
+export ELASTICSEARCH_ENABLED=false # false to disable Elastic
 
-# Start the server
-echo "Starting server..."
+# Set the port for the Node.js server
+export FE_PORT=8001
+
+# Start the Node.js server
+echo "Starting Node.js server..."
+cd frontend && PORT=$FE_PORT node server.js &
+
+# Wait a moment for the Node.js server to start
+sleep 2
+
+# Print the clickable link
+echo "\n\033[0;34mChat widget is available at: \033[4;34mhttp://localhost:$FE_PORT\033[0m"
+
+# Start the Python server
+echo "Starting Python server..."
 exec poetry run gunicorn --config ws/gunicorn_config.py --worker-class eventlet -w 1 app.main:app_instance -b 0.0.0.0:8002
