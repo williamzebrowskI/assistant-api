1a7c23604d60d8ed35f26356c4e081df69b16fd0
William Zebrowski <38896109+williamzebrowskI@users.noreply.github.com>
2024-08-30 21:57:58 -0400

    update elastic connector

--- managers/elastic/es_connector/elastic_connect.py
+++ managers/elastic/es_connector/elastic_connect.py
@@ -11,14 +11,30 @@ class BaseElasticConnector:
     def __init__(self):
         self.es_url = config.config.ES_URL
         self.es_port = config.config.ES_PORT
-        self.es_api_key = config.config.ES_API_KEY
+        self.es_username = config.config.ES_USERNAME
         self.es_index = config.config.ES_INDEX
+        self.es_password = config.config.ES_PASSWORD
 
         try: 
             self.es = Elasticsearch(
-                hosts=[UrlUtility.create_url(f"{self.es_url}:{self.es_port}")],
-                api_key=self.es_api_key
+               "https://elasticsearch:9200",
+                basic_auth=(self.es_username, self.es_password),
+                verify_certs=False,
+                ssl_show_warn=False
             )
+                        # Check if the connection is successful
+            if self.es.ping():
+                print("Successfully connected to Elasticsearch!")
+                logging.info("Successfully connected to Elasticsearch!")
+            else:
+                print("Connected to Elasticsearch, but ping failed.")
+                logging.warning("Connected to Elasticsearch, but ping failed.")
+            
+            # Optionally, you can print more information about the cluster
+            info = self.es.info()
+            print(f"Elasticsearch cluster name: {info['cluster_name']}")
+            print(f"Elasticsearch version: {info['version']['number']}")
+
         except Exception as e:
             logging.error("Failed to connect to Elasticsearch: %s", e)
             raise SystemExit("Connection to Elasticsearch failed: %s" % e)
