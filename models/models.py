65e86d6e97eb4823244ac6de08fee6f6373ee408
William Zebrowski <38896109+williamzebrowskI@users.noreply.github.com>
2024-09-02 13:49:35 -0400

    update models

--- models/models.py
+++ models/models.py
@@ -4,30 +4,16 @@ import uuid
 
 @dataclass
 class User:
-    client_ip: str
-    session_id: str
     user_id: str
-    url: str
     user_query: str
     index: int = None
-    referral_url: str = None
-    intent_data: dict = field(default_factory=dict) 
 
     @classmethod
-    def from_message_data(cls, msg_data, index=None, intent=None, confidence=None):
-        intent_data = {
-            "intent": intent,
-            "confidence": confidence
-        }
+    def from_message_data(cls, msg_data, index=None):
         return cls(
-            client_ip=msg_data.client_ip,
-            session_id=msg_data.session_id_ga,
             user_id=msg_data.user_id,
-            url=msg_data.page_url,
             user_query=msg_data.user_input,
             index=index,
-            referral_url=msg_data.referral_url,
-            intent_data=intent_data
         )
 
 @dataclass
@@ -35,9 +21,6 @@ class AssistantResponse:
     assistant_id: str = None
     thread_id: str = None
     assistant_response: str = None
-    assistant_type: str = 'openAI'
-    start_respond_timestamp: str = None
-    end_respond_timestamp: str = None
     index: int = None
     feedback: dict = field(default_factory=lambda: {
         "feedback_num": 0,
@@ -47,21 +30,13 @@ class AssistantResponse:
 @dataclass
 class Conversation:
     conversation_id: str
-    initial_session_id: str
-    initial_user_id: str
-    assistant_type: str
-    partner_id: str
-    title: str
-    start_timestamp: str = datetime.now().isoformat()
-    last_updated_timestamp: str = start_timestamp
-    debug_stack: list = None
+    user_id: str
     turns: list = None
 
 @dataclass
 class Turn:
     turn_id: str
     conversation_id: str
-    turn_timestamp: str
     user: dict
     assistant: dict
     index: int
@@ -71,7 +46,6 @@ class Turn:
         return cls(
             turn_id=str(uuid.uuid4()),
             conversation_id=conversation_id,
-            turn_timestamp=datetime.now().isoformat(),
             user=asdict(user),
             assistant=asdict(assistant),
             index=index
@@ -84,11 +58,9 @@ class Turn:
                     ctx._source.turns = [];
                 }
                 ctx._source.turns.add(params.turn);
-                ctx._source.last_updated_timestamp = params.last_updated_timestamp;
             """,
             "lang": "painless",
             "params": {
                 "turn": asdict(self),
-                "last_updated_timestamp": datetime.now().isoformat()
             }
         }
