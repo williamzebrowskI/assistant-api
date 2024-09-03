6ca4a1fc5634660732ea7503ad10a34ef20adab8
William Zebrowski <38896109+williamzebrowskI@users.noreply.github.com>
2024-09-03 13:34:18 -0400

    update main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from os import getenv
 getenv('PYTHONDONTWRITEBYTECODE', '1')
 from openai import OpenAI
-from ws.flask_config import config
-from managers.openai.managers.thread_manager import ThreadManager
-from managers.openai.managers.assistant_manager import OpenAIAssistant
-from managers.elastic.convo_managers.conversation_managers import ConversationManager
-from managers.elastic.es_connector.elastic_connect import BaseElasticConnector  
+from app.ws.flask_config import config
+from app.managers.openai.managers.thread_manager import ThreadManager
+from app.managers.openai.managers.assistant_manager import OpenAIAssistant
+from app.managers.elastic.convo_managers.conversation_managers import ConversationManager
+from app.managers.elastic.es_connector.elastic_connect import BaseElasticConnector  
 from dotenv import load_dotenv
 load_dotenv()
 
